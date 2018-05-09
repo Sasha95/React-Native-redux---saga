a516e05c88cac3d0325d0dcc8c2b761d7b6cbb15
Sasha <sashamangir@mail.ru>
2018-05-09 13:51:39 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { AppRegistry } from "react-native";
+
+import { Provider } from "react-redux";
+import configureStore from "./src/configureStore";
+import App from "./src/App";
+
+const store = configureStore();
+
+const ReduxApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent("redux4ways", () => ReduxApp);
